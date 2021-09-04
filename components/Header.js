879bdae3f2d745394986c8c1206397f9f5d5cd7f
AbhishekSagu <abhishek.sagu@antiersolutions.com>
2021-09-04 18:34:06 +0530

    added the header navbar

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,59 @@
-import React from "react";
 import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* LEFT PORTION */}
-      <div>
+      <div className="flex items-center">
         <Image
           src="https://links.papareact.com/5me"
           width={40}
           height={40}
         ></Image>
 
-        <div>
-          <input type="text" placeholder="Search Facebook" />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            type="text"
+            className="hidden md:inline-flex flex ml-2 flex-shrink items-center bg-transparent outline-none placeholder-grey-500"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
-
       {/* CENTER PORTION */}
-
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
       {/* RIGHT PORTION */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* user profile pic */}
+        <p className="whitespace-no-wrap pr-3 font-semibold">SHISUKE URAHARA</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 }
